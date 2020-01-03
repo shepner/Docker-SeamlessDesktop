e6c107179f9757ff61b0ed7396c4a25adaa90beb
Stephen Hepner <5170785+shepner@users.noreply.github.com>
2020-01-03 15:51:17 -0600

    Update start.sh

--- usr/local/bin/start.sh
+++ usr/local/bin/start.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 # Launch the application from here
 
-#https://xpra.org/trac/wiki/Usage/Docker
+# Xpra stuff:
 
-#https://xpra.org/trac/wiki/Clients/HTML5
-#su $ENV_USR -c xpra start :100 --bind-tcp=0.0.0.0:14500 --start-via-proxy=no --start=firefox
+# https://xpra.org/trac/wiki/Usage/Docker
+# https://xpra.org/trac/wiki/Clients/HTML5
 
-#sleep infinity
+su $ENV_USR -c 'xpra start --start-via-proxy=no --bind-tcp=0.0.0.0:14500 --speaker=disabled --microphone=disabled --start=firefox'
+sleep 10 #xpra takes a bit to start up
+
+sleep infinity
