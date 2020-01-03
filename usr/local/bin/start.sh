9980b7d62258dacebd3c5f8ff3650b5522807acd
Stephen Hepner <5170785+shepner@users.noreply.github.com>
2020-01-03 15:02:35 -0600

    Update start.sh

--- usr/local/bin/start.sh
+++ usr/local/bin/start.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 # Launch the application from here
 
-#https://xpra.org/trac/wiki/Clients/HTML5
-xpra start --bind-tcp=0.0.0.0:14500 --start=firefox
+#https://xpra.org/trac/wiki/Usage/Docker
 
-sleep infinity
+#https://xpra.org/trac/wiki/Clients/HTML5
+#su $ENV_USR -c xpra start :100 --bind-tcp=0.0.0.0:14500 --start-via-proxy=no --start=firefox
+
+#sleep infinity
